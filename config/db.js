56cfb48d3ae5357ca947f440ed9255ac967a716a
Himanshu Jaiswal <GI0328@garudaAD.com>
2025-06-23 17:39:08 +0530

    db changed

--- config/db.js
+++ config/db.js
@@ -2,31 +2,31 @@
 
 // // Create the promise-based pool directly
 // const pool = mysql.createPool({
-//   host: '3.108.235.1',
+//   host: 'localhost',
 //   user: 'root',
 //   database: 'fnsolutions',
-//   password: 'fnsAdmin_23062025',
+//   password: '',
 //   waitForConnections: true,
 // });
 
 // module.exports = { promisePool: pool };
 
-const mysql = require('mysql2/promise');
+  const mysql = require('mysql2/promise');
 
-(async () => {
-  try {
-    const connection = await mysql.createConnection({
-      host: 'fnsolutionsdb.clkwcssiqip4.ap-south-1.rds.amazonaws.com',
-      user: 'admin',
-      password: 'md1d9M6f0D2fPEEPPZgT',
-      database: 'fnsolutions',
-    });
+  (async () => {
+    try {
+      const connection = await mysql.createConnection({
+        host: 'fnsdatabase.clkwcssiqip4.ap-south-1.rds.amazonaws.com',
+        user: 'admin',
+        password: '5NCVAXM4HqWFkgejQTO4',
+        database: 'fnsolutions',
+      });
 
-    const [rows] = await connection.execute('SELECT NOW()');
-    console.log('Connected! Current time:', rows[0]);
+      const [rows] = await connection.execute('SELECT NOW()');
+      console.log('Connected! Current time:', rows[0]);
 
-    await connection.end();
-  } catch (err) {
-    console.error('Connection failed:', err);
-  }
-})();
+      await connection.end();
+    } catch (err) {
+      console.error('Connection failed:', err);
+    }
+  })();
